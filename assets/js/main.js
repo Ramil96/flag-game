967c4f64691275127b1c3428f96cd49539a63dce
Ramil Tuazon <ramilytuazon@gmail.com>
2024-10-06 17:03:50 +0000

    add function to display dnamic flag change options

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,13 @@
 let countSpan = document.querySelector('.count span')
 function getQuestions(){
     let myRequest = new XMLHttpRequest()
-    myRequest.onstatechange = function (){
+    myRequest.onreadystatechange = function (){
         if(this.readyState === 4 && this.status === 200) {
             let questions = JSON.parse(this.responseText);
             console.log(questions);
             // number of questions each new game
             let qCount = 10;
+            questionNum(qCount);
         }
     }
     myRequest.open("GET", "assets/data/flag_questions.json", true)
@@ -15,3 +16,7 @@ function getQuestions(){
 }
 
 getQuestions();
+
+function questionNum(num) {
+    countSpan.innerHTML = num;
+}
