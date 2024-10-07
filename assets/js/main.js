a4b1acb7bc7400fb0b332d3062dc2b97007db555
Ramil Tuazon <ramilytuazon@gmail.com>
2024-10-07 01:35:05 +0000

    code: fix code typos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,50 @@
 // select elements
-let countSpan = document.querySelector('.count span')
-let flagImgDiv = document.querySelector('.flag-img')
-let flagImg = document.querySelector('.flag-img img')
+let countSpan = document.querySelector('.count span');
+let flagImgDiv = document.querySelector('.flag-img');
+let flagImg = document.querySelector('.flag-img img');
 let flagOptions = document.querySelector('.flag-options ul');
 let flagLis = document.querySelectorAll('.flag-options ul li');
-let score = document.querySelector(`h3 span`);
-let scoreDiv = document.querySelector(`.score`);
-let correctAns = document.querySelector(`.score .right span`);
-let incorrectAns = document.querySelector(`.score .wrong span`);
-let btnNewGame = document.querySelector(`#newGame`);
+let score = document.querySelector('h3 span');
+let scoreDiv = document.querySelector('.score');
+let correctAns = document.querySelector('.score .right span');
+let incorrectAns = document.querySelector('.score .wrong span');
+let btnNewGame = document.querySelector('#newGame');
 
 let currentIndex = 0;
 let rightAnswer = 0;
 
-function getQuestions(){
-    let myRequest = new XMLHttpRequest()
-    myRequest.onreadystatechange = function (){
-        if(this.readyState === 4 && this.status === 200) {
+function getQuestions() {
+    let myRequest = new XMLHttpRequest();
+    myRequest.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
             let questions = JSON.parse(this.responseText);
             console.log(questions);
             // number of questions each new game
             let qCount = 10;
             questionNum(qCount);
-            //Add questions data
+            // Add questions data
             addQuestionData(questions[currentIndex], qCount);
 
-            flagLis.forEach(li => {
+            // Add click event to each option
+            flagLis.forEach((li) => {
                 li.addEventListener('click', () => {
+                    let rightAnswer = questions[currentIndex].right_answer;
+                    // Remove 'active' class from all options
+                    flagLis.forEach((item) => item.classList.remove('active'));
                     li.classList.add('active');
-                })
-            })
+                    
+                    // Increase index for the next question
+                    currentIndex++;
+
+                    // Check the answer after 500ms
+                    setTimeout(() => {
+                        checkAnswer(rightAnswer, qCount);
+                    }, 500);
+                });
+            });
         }
-    }
-    myRequest.open("GET", "assets/data/flag_questions.json", true)
+    };
+    myRequest.open('GET', 'assets/data/flag_questions.json', true);
     myRequest.send();
 }
 
@@ -43,32 +55,34 @@ function questionNum(num) {
 }
 
 function addQuestionData(obj, count) {
-    if(currentIndex < count) {
+    if (currentIndex < count) {
         flagImg.src = `assets/images/${obj.img}`;
-        //Create Options
+        // Create Options
         flagLis.forEach((li, i) => {
-            //Give each Li a dynamic Id
-            li.id = `answer_${i+1}`;
-            //Create for each Li a dynamic data-attribute
-            li.dataset.answer=obj[`options`][i];
-            //Inserting option in the li
-            li.innerHTML = obj[`options`][i];
+            // Give each li a dynamic ID
+            li.id = `answer_${i + 1}`;
+            // Create for each li a dynamic data-attribute
+            li.dataset.answer = obj.options[i];
+            // Inserting option in the li
+            li.innerHTML = obj.options[i];
+            // Clear previous classes
+            li.classList.remove('active', 'success', 'wrong');
         });
     }
 }
 
 function checkAnswer(rAnswer, count) {
     let chosenAnswer;
-    for (let i = 0; i < flagLis.length; i++){
-        if(flagLis[i].classList.contains(`active`)){
-            chosenAnswer = flagLis[i].dataset.answer;
-            if(rAwnser === chosenAnswer){
-                flagLis[i].classList.add(`success`);
+    flagLis.forEach((li) => {
+        if (li.classList.contains('active')) {
+            chosenAnswer = li.dataset.answer;
+            if (rAnswer === chosenAnswer) {
+                li.classList.add('success'); // Correct answer
                 rightAnswer++;
-                score.innerHTML = rightAnswer; 
-            }   else {
-                flagLis[i].classList.add(`wrong`);
+                score.innerHTML = rightAnswer; // Update score
+            } else {
+                li.classList.add('wrong'); // Wrong answer
             }
         }
-    }
+    });
 }
